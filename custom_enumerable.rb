74ae2d92353d0a0339aad603e122e7a9fb5c3abf
tta2yta <tta2yta@gmail.com>
2021-02-17 18:32:15 +0300

    Implemented my_each method

--- custom_enumerable.rb
+++ custom_enumerable.rb
@@ -1,4 +1,13 @@
 module Enumerable
     def my_each
+        if block_given?
+            i=0
+            while i < self.size
+                yield self[i]
+                i += 1
+            end
+        else
+            yield self
+        end
     end
 end
