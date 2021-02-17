4a6cf34aaed53ecbf9f7d5e36c55eb0e5d2834ec
tta2yta <tta2yta@gmail.com>
2021-02-17 19:14:24 +0300

    Fixed Linters

--- custom_enumerable.rb
+++ custom_enumerable.rb
@@ -1,20 +1,20 @@
 module Enumerable
-    # custom enumerable method that resembles buit-in my_each enumerable
-    def my_each
-        if block_given?
-            i=0
-            while i < self.size
-                yield self[i]
-                i += 1
-            end
-        else
-            yield self
-        end
+  # custom enumerable method that resembles buit-in my_each enumerable
+  def my_each
+    if block_given?
+      i = 0
+      while i < size
+        yield self[i]
+        i += 1
+      end
+    else
+      yield self
     end
+  end
 end
+num = [1, 2, 3, 4, 5]
+str = %w[a b c d]
 
-num=[1,2,3,4,5]
-str=['a','b','c','d']
-
-#calling my_each enumerable
-num.my_each{|x| puts "x=#{x}"}
+# calling my_each enumerable
+num.my_each { |x| puts "x=#{x}" }
+str.my_each { |y| puts "y=#{y}" }
