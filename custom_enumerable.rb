d716aab074403974517ff51afcb96add11747496
tta2yta <tta2yta@gmail.com>
2021-02-18 15:38:09 +0300

    Called/Tested my_select method

--- custom_enumerable.rb
+++ custom_enumerable.rb
@@ -4,6 +4,8 @@ module Enumerable
     if block_given?
       i = 0
       while i < size
+        break if i == size
+
         yield self[i]
         i += 1
       end
@@ -25,11 +27,15 @@ module Enumerable
     end
   end
 
-  def my_select
-  
+  def my_select(&block)
+    result = []
+    my_each do |x|
+      result << x if block.call(x)
+    end
+    result
   end
 end
-num = [1, 2, 3, 4, 5]
+num = [5, 7, 3, 4, 5]
 str = %w[a b c d]
 
 # calling my_each enumerable
@@ -39,3 +45,6 @@ str.my_each { |y| puts "y=#{y}" }
 # calling my_each_with_index enumerable
 num.my_each_with_index { |index, item| puts "index=#{index}, item=#{item}" }
 str.my_each_with_index { |index, item| puts "index=#{index}, item=#{item}" }
+
+# calling my_select enumerable
+p(num.my_select { |x| x > 3 })
