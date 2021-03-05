30f95a93a6c8f607775f2ca51badd119b1686ef4
tta2yta <tta2yta@gmail.com>
2021-03-05 11:20:09 +0300

    Test my_each no block given

--- spec/custom_enumerable_rspec.rb
+++ spec/custom_enumerable_rspec.rb
@@ -1,3 +1,14 @@
 # spec/custom_enumerable_rspec.rb
+require './custom_enumerables.rb'
 
+describe Enumerable do
+  let(:arr) { [3, 5, 7, 2, 1] }
+  let(:ran) { (1..10) }
+  let(:board) { { 'name': 'AAA', 'age': 30, 'gender': 'male' } }
 
+  describe '#my_each' do
+    it 'return enumerable if block is not given' do
+      expect(arr.my_each).to be_an_instance_of(Enumerator)
+    end
+  end
+end
