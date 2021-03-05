632ac0c8ad5eac532816e81d0508ac2d1827e1b6
luqmanmusah <musahluqman@gmail.com>
2021-03-05 09:40:21 +0000

    merged

--- spec/custom_enumerable_rspec.rb
+++ spec/custom_enumerable_rspec.rb
@@ -5,6 +5,7 @@ describe Enumerable do
   let(:arr) { [3, 5, 7, 2, 1] }
   let(:ran) { (1..10) }
   let(:person) { { 'name': 'AAA', 'age': 30, 'gender': 'male' } }
+  let(:hash) { {} }
 
   describe '#my_each' do
     it 'return enumerable if block is not given' do
@@ -21,6 +22,30 @@ describe Enumerable do
   
       it 'returns the original hash once it is done with the block' do
         expect(person.my_each { |country| "country - #{country} " }).to eql(person)
+      end
+  end
+  describe '#my_each_with_index' do
+    it 'return enumerable if block is not given' do
+      expect(arr.my_each_with_index).to be_an_instance_of(Enumerator)
+    end
+    it 'returns the original array once it is done with the block' do
+        expect(arr.my_each_with_index { |x| x * 2 }).to eql(arr)
+      end
+  
+      it 'returns the original range once it is done with the block' do
+        expect(ran.my_each_with_index { |x| x * 2 }).to eql(ran)
+      end
+  
+      it 'returns the original hash once it is done with the block' do
+        expect(person.my_each_with_index { |country| "country - #{country} " }).to eql(person)
+      end
+      it 'iterates over a given array while retriving the index' do
+        arr.my_each_with_index { |numbers, index| hash[numbers] = index }
+        expect(hash).to eql(3 => 0, 5 => 1, 7 => 2, 2 => 3, 1 => 4)
+      end
+      it 'iterates over a given hash while retriving the index' do
+        person.my_each_with_index { |user, index| hash[user] = index }
+        expect(hash).to eql( [name: AAA]=> 0, [age: 30] => 1, [gender: male] => 2)
       end
   end
 end
