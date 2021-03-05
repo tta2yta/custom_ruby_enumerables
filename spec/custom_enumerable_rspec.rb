1ac2748fbb7815da312b98d17e7c207fc9472b8f
tta2yta <tta2yta@gmail.com>
2021-03-05 12:52:21 +0300

    Test my_seclect when block is given and not

--- spec/custom_enumerable_rspec.rb
+++ spec/custom_enumerable_rspec.rb
@@ -12,7 +12,7 @@ describe Enumerable do
       expect(arr.my_each).to be_an_instance_of(Enumerator)
     end
     it 'returns the original array once it is done with the block' do
-        arr.my_each { |x| puts x * 2 }
+        arr.my_each { |x| x * 2 }
         expect(arr.my_each { |x| x * 2 }).to eql(arr)
       end
   
@@ -29,23 +29,25 @@ describe Enumerable do
       expect(arr.my_each_with_index).to be_an_instance_of(Enumerator)
     end
     it 'returns the original array once it is done with the block' do
-        expect(arr.my_each_with_index { |x| x * 2 }).to eql(arr)
+        expect(arr.my_each_with_index { |x, index| x * 2 + index}).to eql(arr)
       end
   
       it 'returns the original range once it is done with the block' do
-        expect(ran.my_each_with_index { |x| x * 2 }).to eql(ran)
+        expect(ran.my_each_with_index { |x, index| x * 2 }).to eql(ran)
       end
   
       it 'returns the original hash once it is done with the block' do
-        expect(person.my_each_with_index { |country| "country - #{country} " }).to eql(person)
-      end
-      it 'iterates over a given array while retriving the index' do
-        arr.my_each_with_index { |numbers, index| hash[numbers] = index }
-        expect(hash).to eql(3 => 0, 5 => 1, 7 => 2, 2 => 3, 1 => 4)
-      end
-      it 'iterates over a given hash while retriving the index' do
-        person.my_each_with_index { |user, index| hash[user] = index }
-        expect(hash).to eql( [name: AAA]=> 0, [age: 30] => 1, [gender: male] => 2)
+        expect(person.my_each_with_index { |country, index| "country - #{country} " }).to eql(person)
       end
   end
+
+  describe "#my_select" do
+  it 'return enumerable if block is not given' do
+    expect(arr.my_select).to be_an_instance_of(Enumerator)
+  end
+
+  it 'Returns an array containing all elements of enum for which the given block returns a true value' do
+    expect(arr.my_select { |x| x > 3 }).to eql([5, 7])
+  end
+end
 end
